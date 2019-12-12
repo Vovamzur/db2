4326e78ab2e264992328cb10808fed978a2f478a
Vovamzur <vovamazur3@gmail.com>
2019-12-13 01:04:58 +0200

    refact campus worker

--- client/src/containers/CampusWorkerPage/index.jsx
+++ client/src/containers/CampusWorkerPage/index.jsx
@@ -1,14 +1,4 @@
 import React from 'react';
 import InfoMenuBar from './tabs/InfoMenuBar';
-import ActionMenuBar from './tabs/ActionMenuBar';
 
-const CampusWorkerPage = () => {
-  return (
-    <div>
-      <InfoMenuBar/>
-      <ActionMenuBar/>
-    </div>
-  )
-};
-
-export default CampusWorkerPage;
+export default () => <InfoMenuBar/>
--- client/src/containers/CampusWorkerPage/tabs/InfoMenuBar.jsx
+++ client/src/containers/CampusWorkerPage/tabs/InfoMenuBar.jsx
@@ -1,83 +1,20 @@
-import React, { Component } from 'react';
-import { Menu, Item } from 'semantic-ui-react'
+import React from 'react';
+import { Tab } from 'semantic-ui-react'
 
 import HostelInfo from './infoElements/HostelInfo';
 import HostelResidentInfo from './infoElements/HostelResidentInfo';
 import ShowInventory from './infoElements/ShowInventory';
 import ShowChecks from './infoElements/ShowChecks';
 import CountPayments from './infoElements/CountPayments';
+import AddHostelResident from './actionElements/AddHostelResident'
 
+const panes = [
+    { menuItem: 'Hostel info', render: () => <Tab.Pane><HostelInfo /></Tab.Pane> },
+    { menuItem: 'Hostel residents info', render: () => <Tab.Pane><HostelResidentInfo /></Tab.Pane> },
+    { menuItem: 'Inventories', render: () => <Tab.Pane><ShowInventory /></Tab.Pane> },
+    { menuItem: 'Cheques', render: () => <Tab.Pane><ShowChecks /></Tab.Pane> },
+    { menuItem: 'Payments', render: () => <Tab.Pane><CountPayments /></Tab.Pane> },
+    { menuItem: 'Add hostel resident', render: () => <Tab.Pane><AddHostelResident /></Tab.Pane> },
+]
 
-class InfoMenuBar extends Component {
-    constructor() {
-        super();
-        this.state = {
-            activeItem: undefined
-        }
-    }
-
-    onMenuClick = (e, { name }) => {
-        this.setState({ activeItem: name });
-    }
-
-    render() {
-        const { activeItem } = this.state;
-
-        return (
-            <div>
-                <Item.Group>
-                    <Item>
-                        <Item.Content>
-                            <Item.Header><h1>Info Menu</h1></Item.Header>
-                        </Item.Content>
-                    </Item>
-                </Item.Group>
-                <Menu>
-                    <Menu.Item
-                        name='hostelInfo'
-                        active={activeItem === 'hostelInfo'}
-                        onClick={this.onMenuClick}
-                    >
-                        <h2>Hostels Info</h2>
-                    </Menu.Item>
-                    <Menu.Item
-                        name='hostelResidentInfo'
-                        active={activeItem === 'hostelResidentInfo'}
-                        onClick={this.onMenuClick}
-                    >
-                        <h2>Hostel Residents Info</h2>
-                    </Menu.Item>
-                    <Menu.Item
-                        name='showInventory'
-                        active={activeItem === 'showInventory'}
-                        onClick={this.onMenuClick}
-                    >
-                        <h2>Show Inventory</h2>
-                    </Menu.Item>
-                    <Menu.Item
-                        name='showChecks'
-                        active={activeItem === 'showChecks'}
-                        onClick={this.onMenuClick}
-                    >
-                        <h2>Show Checks</h2>
-                    </Menu.Item>
-                    <Menu.Item
-                        name='countPayments'
-                        active={activeItem === 'countPayments'}
-                        onClick={this.onMenuClick}
-                    >
-                        <h2>Count Payments</h2>
-                    </Menu.Item>
-                </Menu>
-                {activeItem === 'hostelInfo' ? <HostelInfo /> : null}
-                {activeItem === 'hostelResidentInfo' ? <HostelResidentInfo /> : null}
-                {activeItem === 'showInventory' ? <ShowInventory /> : null}
-                {activeItem === 'showChecks' ? <ShowChecks /> : null}
-                {activeItem === 'countPayments' ? <CountPayments /> : null}
-
-            </div >
-        )
-    }
-}
-
-export default InfoMenuBar;
+export default () => <Tab panes={panes} />;
