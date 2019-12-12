dc53a4251799cf12f6e68c00affd981c3a16136f
Vovamzur <vovamazur3@gmail.com>
2019-12-12 03:44:20 +0200

    create tabs for deanery page

---
+++ client/src/containers/DeaneryPage/tabs/SessionSchedule.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// subject, data, name, aacdemic, 
+const schedule = [];
+// add, update, delete, watch
+
+const SessionSchedule = () => {
+  return (
+    <div>
+      SessionSchedule
+    </div>
+  )
+}
+
+export default SessionSchedule;
